6c8a09ae66be772ed7335b44b940a97373eded23
plugkevo <munenekevin366@gmail.com>
2023-04-26 15:45:55 +0300

    contact us done

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <title>Bootstrap Gridlayout</title>
     <link rel="stylesheet" href="bootstrap-5.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
 
 </head>
 <body>
